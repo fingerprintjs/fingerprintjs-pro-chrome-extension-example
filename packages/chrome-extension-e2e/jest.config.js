c822b38374ed8873d89e106eec0f64bbc7f7b464
Przemysław Żydek <przemyslawzydek@gmail.com>
2022-05-05 13:58:57 +0200

    ci: add unit test result action

--- packages/chrome-extension-e2e/jest.config.js
+++ packages/chrome-extension-e2e/jest.config.js
@@ -6,6 +6,10 @@ module.exports = {
   testRegex: 'tests/.+spec.tsx?$',
   testTimeout: 999999,
   setupFilesAfterEnv: [path.resolve(__dirname, 'tests/setup.ts')],
+  reporters: [
+    'default',
+    ['jest-junit', { outputDirectory: 'reports', outputName: 'report.xml' }],
+  ],
   globals: {
     'ts-jest': {
       tsconfig: path.resolve(__dirname, 'tsconfig.json'),
