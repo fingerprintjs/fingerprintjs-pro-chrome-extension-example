89fb8bf8d1b4d541c0de87d616f38ca2092651a4
Przemysław Żydek <przemyslawzydek@gmail.com>
2022-05-05 13:36:26 +0200

    test: add retry times

--- packages/chrome-extension-e2e/tests/setup.ts
+++ packages/chrome-extension-e2e/tests/setup.ts
@@ -2,6 +2,8 @@ import { cleanup, createBrowser } from './browser';
 import { config } from 'dotenv';
 import * as path from 'path';
 
+jest.retryTimes(3);
+
 config({
   path: path.resolve(__dirname, '../../../.env'),
 });
