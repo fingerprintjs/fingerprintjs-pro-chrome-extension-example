b6cad3cc6aab79e6e79e40cbfb9e2fc3c95e569a
Przemysław Żydek <przemyslawzydek@gmail.com>
2022-05-09 14:17:54 +0200

    build: update releases config

---
+++ packages/chrome-extension/scripts/syncManifestVersion.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const path = require('path');
+
+const packageJson = JSON.parse(
+  fs.readFileSync(path.join(__dirname, '../package.json'), 'utf8')
+);
+
+const manifestPaths = [path.resolve(__dirname, '../manifest.json')];
+
+manifestPaths.forEach(manifestPath => {
+  if (!fs.existsSync(manifestPath)) {
+    return;
+  }
+
+  const manifest = JSON.parse(fs.readFileSync(manifestPath).toString());
+
+  const { version, versionName } = parseVersion(packageJson.version);
+
+  manifest.version = version;
+  manifest.version_name = versionName;
+
+  fs.writeFileSync(manifestPath, JSON.stringify(manifest, null, 2));
+});
+
+function parseVersion(version) {
+  const safeVersion = version.split('-')[0];
+
+  return {
+    version: safeVersion,
+    versionName: version,
+  };
+}
