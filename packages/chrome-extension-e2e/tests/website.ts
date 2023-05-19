8ed757dfe0fde12f122c9ac53bdb6edccbc5f7a3
Przemysław Żydek <przemyslawzydek@gmail.com>
2023-05-19 17:33:05 +0900

    chore: log attempt

--- packages/chrome-extension-e2e/tests/website.ts
+++ packages/chrome-extension-e2e/tests/website.ts
@@ -17,6 +17,8 @@ export async function startWebsite(extensionId: string) {
 }
 
 export async function waitForWebsite() {
+  let attempts = 0;
+
   const httpsAgent = new https.Agent({
     rejectUnauthorized: false,
   });
@@ -24,6 +26,10 @@ export async function waitForWebsite() {
   await wait(1500);
 
   while (true) {
+    if (attempts > 0) {
+      console.log(`Waiting for website to be ready... (${attempts})`);
+    }
+
     try {
       const websiteUrl = process.env.WEBSITE_URL as string;
 
@@ -35,8 +41,10 @@ export async function waitForWebsite() {
         return true;
       }
     } catch (error) {
-      // Nothing here
+      console.error(error);
     }
+
+    attempts++;
 
     await wait(1000);
   }
