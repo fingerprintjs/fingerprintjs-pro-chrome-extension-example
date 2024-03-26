2a56ed7fbff5d31f54cbae6f0a8c10a306d7ec9a
Przemysław Żydek <przemyslawzydek@gmail.com>
2024-03-26 13:20:53 +0900

    ci: use shared workflows

--- packages/chrome-extension/webpack.config.js
+++ packages/chrome-extension/webpack.config.js
@@ -12,6 +12,10 @@ dotenv.config({
   path: path.resolve(__dirname, '../../.env'),
 })
 
+dotenv.config({
+  path: path.resolve(__dirname, '../../.env.dist'),
+})
+
 function parseVersion(version) {
   const safeVersion = version.split('-')[0]
 
